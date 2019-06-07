deac5678a8dc51523f023d6c220223b17f545460
GoastShadow <182136231@qq.com>
2019-06-07 20:06:18 +0800

    初始配置initali

---
+++ src/page/index/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: Miss.chenfang 
+ * @Date: 2019-06-03 14:56:46 
+ * @Last Modified by: chenjie
+ * @Last Modified time: 2019-06-04 15:06:11
+ */
+
+
+'use strict';
+require('./index.css');
+require('../module.js');
---
+++ src/page/module.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: Miss.chenfang 
+ * @Date: 2019-06-04 11:37:45 
+ * @Last Modified by: chenjie
+ * @Last Modified time: 2019-06-04 11:42:14
+ */
+'use strict';
+
+module.exports = {
+    test: 123
+}
